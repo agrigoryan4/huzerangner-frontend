417cd13e3e38762d6321d3a9d224819ebb66f018
WinterFirebird <naprimerarman@gmail.com>
2021-02-26 15:11:41 +0400

    :bug: bug and typo fixes

--- src/components/Content/PostSingle/ShareSingle.js/ShareSingle.js
+++ src/components/Content/PostSingle/ShareSingle.js/ShareSingle.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   FacebookShareButton,
-  PinterestShareButton,
   TelegramShareButton,
   TwitterShareButton,
   ViberShareButton,
@@ -12,14 +11,13 @@ import {
   TwitterIcon,
   WhatsappIcon,
   ViberIcon,
-  PinterestIcon,
   TelegramIcon
 } from "react-share";
 
 const iconSize = (window.innerWidth > 480) ? 48 : 32;
 
 const ShareSingle = ({ title, url }) => {
-  const titleFormatted = `«${title}» \nՀուզերանգներ | Հ․ Ղուկասյանը հեղինակային բլոգը`;
+  const titleFormatted = `«${title}» \nՀուզերանգներ | Հ․ Ղուկասյանի հեղինակային բլոգը`;
   // const url = 'https://huzerangner.netlify.app/posts/post/60364b9766d5a32854cd376b';
   return (
     <div>
@@ -31,21 +29,13 @@ const ShareSingle = ({ title, url }) => {
         <TwitterIcon size={iconSize} />
       </TwitterShareButton>
 
-      <WhatsappShareButton
-        title={titleFormatted}
-        url={url}
-        separator=":: "
-      >
+      <WhatsappShareButton title={titleFormatted} url={url} separator=" ">
         <WhatsappIcon size={iconSize} />
       </WhatsappShareButton>
 
       <ViberShareButton title={titleFormatted} url={url}>
         <ViberIcon size={iconSize} />
       </ViberShareButton>
-
-      <PinterestShareButton title={titleFormatted} url={url}>
-        <PinterestIcon size={iconSize} />
-      </PinterestShareButton>
 
       <TelegramShareButton title={titleFormatted} url={url}>
         <TelegramIcon size={iconSize} />
