1647f03797a7477926a7d2e3042a1350df725156
WinterFirebird <naprimerarman@gmail.com>
2021-02-28 23:53:56 +0400

    :bug:

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { POSTS_PER_PAGE } from '../constants';
-const rootURL = 'https://huzerangner.heroku.com/';
+const rootURL = 'https://huzerangner.heroku.com';
 
 export const getPostSingle = (postId) => axios.get(`${rootURL}/posts/post/${postId}`);
 export const getPosts = (page, query) => {
